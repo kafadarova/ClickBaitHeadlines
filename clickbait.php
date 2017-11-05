379c45b69182d8d49ff50496def8bdffd3d5b51d
Kristin Kafadarova <k.kafadarova@gmail.com>
2017-11-05 21:58:56 +0100

    Debugging + all the php functions were stored in the new file - functions.php

--- clickbait.php
+++ clickbait.php
@@ -1,33 +1,56 @@
 <?php
+define('TITLE', 'Honest Click Bait Healines');
+include ('functions.php');
 
+if( isset( $_POST["fix_submit"] ) ) {
+  // call function
+  checkForClickBait();
+}
  ?>
- <!DOCTYPE html>
- <html>
-   <head>
 
-     <meta charset="utf-8">
-     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html>
+  <head>
 
-     <title><?php echo TITLE; ?></title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo TITLE; ?></title>
 
-     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  </head>
-   <body>
-<div class="container">
-  <h1><?php TITLE; ?></h1>
-  <p class="lead">Hate click bait? Turn those annoying headlines into realist and honest ones using this simple program.</p>
+    <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+ </head>
 
-  <div class="row">
-    <form class="col-sm-8 col-sm-offset-2" action="" method="post">
-<textarea class="form-control input-lg" name="clickbait_headline" placeholder="Paste click bait headline here"></textarea>
-<button type="submit" class="btn btn-primary btn-lig pull-right" name="fix_submit">Make honest</button>
-    </form>
+  <body>
+ <div class="container">
+ <h1><?php TITLE; ?></h1>
+ <p class="lead">Hate click bait? Turn those annoying headlines into realist and honest ones using this simple program.</p>
 
-  </div>
-</div>
+ <div class="row">
+   <form class="col-sm-8 col-sm-offset-2" action="" method="post">
+ <textarea class="form-control input-lg" name="clickbait_headline" placeholder="Paste click bait headline here"></textarea>
+ <button type="submit" class="btn btn-primary btn-lig pull-right" name="fix_submit">Make honest</button>
+   </form>
+ </div>
 
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    </body>
-  </html>
+ <?php
+if (isset($_POST["fix_submit"])) {
+  //get first value in array by checkForClickBait() function
+  //store it in a variable
+  $clickBait = checkForClickBait()[0];
+
+  //get second value in array by checkForClickBait() function
+  //store it in a variable
+  $honestHeadline = checkForClickBait()[1];
+
+  //call function
+  //pass two arguments in the function
+  displayHonestHeadline($clickBait, $honestHeadline);
+}
+
+  ?>
+ </div>
+
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+   </body>
+ </html>
